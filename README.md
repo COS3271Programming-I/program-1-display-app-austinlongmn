39996be1b8d840fd999b8d26f2a4368f460fadb6
Austin <137583592+austinlongmn@users.noreply.github.com>
2025-08-30 11:02:22 -0500

    Wrote Display APP

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/t6Ae8o-l)
+# Program #1: Display APP
+
+## Sample Run
+
+```
+Where the Spirit of the Lord is,
+                       there is FREEDOM.
+	2 Corinthians 3:17b
+```
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
+  public static void main(String[] args) {
+    // Output message
+    System.out.println("Where the Spirit of the Lord is,");
+
+    // Line up "is" with the above message
+    System.out.format("%31s %s", "there is", "freedom.\n".toUpperCase());
+
+    // Display reference with tab
+    System.out.println("\t2 Corinthians 3:17b");
+  }
 }
